c19fcd0c7998e58de8bb6c3b3f13f451f9307579
Andrew Le <s3820098@rmit.edu.vn>
2022-04-13 09:40:34 +0700

    4. Add usage in React Native

---
+++ usgage_in_react_native/App.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <WebView
+                allowsInlineMediaPlayback={true}
+                source={{
+                    uri: 'https://drewindeed.github.io/test-qr-scan.github.io/',
+                }}
+                startInLoadingState={true}
+                onShouldStartLoadWithRequest={(event) => {
+                    if (
+                        event.url !==
+                        'https://drewindeed.github.io/test-qr-scan.github.io/'
+                    ) {
+                        // print out the url when scanned QR code successfully
+                        console.log('onShouldStartLoadWithRequest', event.url);
+                        
+                        // Code to use the url further goes here.
+
+                        return false;
+                    }
+                    return true;
+                }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
